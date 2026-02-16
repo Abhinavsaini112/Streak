537f2fd744868d7f2bafcd902698c44fbda581f4
Abhinavsaini112 <abhinavsaini8439@gmail.com>
2026-02-17 00:23:56 +0530

    Day 4 Streak

--- Leetcode/Medium/ReversewordsString.py
+++ Leetcode/Medium/ReversewordsString.py
@@ -7,3 +7,23 @@ def ReverseWordsString(s):
 s = "The sky is blue"
 result = ReverseWordsString(s)
 print(result)
+
+
+def ReverseWordsString(s):
+    words = []
+    n = len(s)
+    i = 0 
+    while i < n:
+        while i < n and s[i] == ' ':
+            i += 1
+        if i < n:
+            word_start = i
+            while i < n and s[i] != " ":
+                i += 1
+            words.append(s[word_start: i])
+    words.reverse()
+    return ' '.join(words)
+
+s = "The sky is blue"
+result = ReverseWordsString(s)
+print(result)
