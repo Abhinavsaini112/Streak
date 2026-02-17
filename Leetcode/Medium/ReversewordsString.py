799e642a646e0f0626570e511442ff7153e44ea4
Abhinavsaini112 <abhinavsaini8439@gmail.com>
2026-02-17 23:49:55 +0530

    Day 4 second commit

--- Leetcode/Medium/ReversewordsString.py
+++ Leetcode/Medium/ReversewordsString.py
@@ -27,3 +27,42 @@ def ReverseWordsString(s):
 s = "The sky is blue"
 result = ReverseWordsString(s)
 print(result)
+
+def RevereseWordsString(s):
+    chars = list(s)
+    n = len(s)
+    
+    def reverse(l,r):
+        while l < r:
+            chars[l], chars[r] =  chars[r], chars[l]
+            l += 1 
+            r -= 1
+    # reverse the string
+    reverse(0,n-1)
+    write = 0 
+    read = 0
+    first_word = False
+    while read < n:
+        # skip spaces
+        while read < n and chars[read] == ' ':
+            read += 1
+        if read == n:
+            break
+        # add space after every word apart from the first word
+        if first_word:
+            chars[write] = ' '
+            write += 1
+        first_word = True
+
+        word_start = write
+
+        while read < n and chars[read] != '':
+            chars[write] = chars[read]
+            write += 1
+            read += 1
+        reverse(word_start , write-1)
+    return ''.join(chars[:write])
+
+s = " The    sky   is blue   "
+result = ReverseWordsString(s)
+print(result)
