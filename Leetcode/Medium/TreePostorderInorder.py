cb2b3bc50490c2d7661fa8b1ae9634cdad72a449
Abhinavsaini112 <abhinavsaini8439@gmail.com>
2026-03-09 23:51:12 +0530

    Continuity

---
+++ Leetcode/Medium/TreePostorderInorder.py
@@ -0,0 +1,36 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def buildTree(inorder, postorder):
+
+    # hashmap to store index of inorder values
+    inorder_map = {val: idx for idx, val in enumerate(inorder)}
+    
+    post_index = len(postorder) - 1
+
+    def helper(left, right):
+        nonlocal post_index
+        
+        if left > right:
+            return None
+        
+        # root value from postorder
+        root_val = postorder[post_index]
+        post_index -= 1
+        
+        root = TreeNode(root_val)
+
+        # find root in inorder
+        index = inorder_map[root_val]
+
+        # IMPORTANT: build right subtree first
+        root.right = helper(index + 1, right)
+        root.left = helper(left, index - 1)
+
+        return root
+
+    return helper(0, len(inorder) - 1)
