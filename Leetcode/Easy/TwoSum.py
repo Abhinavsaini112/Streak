337a9f25bedbc7430cf54b062f3a214f7cde1671
Abhinavsaini112 <abhinavsaini8439@gmail.com>
2026-03-12 08:31:18 +0530

    streak

--- Leetcode/Easy/TwoSum.py
+++ Leetcode/Easy/TwoSum.py
@@ -7,7 +7,7 @@ def TwoSum(nums:List[int],target:int)->List[int]:
         if difference in pair:
             return [pair[difference],index]
         pair[value]=index
-   
+    return ["Sum Not Found"]
 
 nums = [2,7,11,15]
 target = 9
