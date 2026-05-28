81ab1820182625e96787cb02813a13cbb5e63149
abhinavsaini112 <abhinavsaini8439@gmail.com>
2026-05-28 08:54:38 +0530

    Day 1 of 21 day challenge

---
+++ Leetcode/HardLevel/Combinations.py
@@ -0,0 +1,17 @@
+def combinations(nums,k):
+    result = []
+
+    def backtracking(start,path):
+        if len(path) == k:
+            result.append(path[:])
+            return 
+        
+        for i in range(start , len(nums)):
+            path.append(nums[i])
+            backtracking(i+1,path)
+            path.pop()
+
+    backtracking(0 , [])
+    return result
+
+print(combinations([1,2,3,4],3))
