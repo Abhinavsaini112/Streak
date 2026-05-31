987631ffcd3360e09cecc3dc359e7737c3002974
abhinavsaini112 <abhinavsaini8439@gmail.com>
2026-05-31 23:12:42 +0530

    Day 4 of 21 Days Challenge

---
+++ Leetcode/Maps.py/Maximum_Frequency.py
@@ -0,0 +1,19 @@
+def Maximum_Frequency(arr):
+    ele = None
+    count = 0
+    result = {}
+    for i in arr:
+        result[i] = result.get(i,0) + 1
+
+        if result[i] > count :
+            count = result[i]
+            ele = i
+
+    return ele
+
+array = [2,12,2,11,12,2,1,2,2,11,12,6]
+element = Maximum_Frequency(array)
+print(element)
+             
+
+
---
+++ Leetcode/Maps.py/PrintKFreqWords.py
@@ -0,0 +1,17 @@
+def Freq_k(arr,k):
+    li = []
+    result = {}
+    words = arr.split()
+    for i in words:
+        result[i] = result.get(i,0) + 1
+    for key in result:
+        if result[key] == k :
+            li.append(key)
+    '''or
+    return [word for word,count in result.items() if count == k ]
+    '''
+    return li
+
+array = "this is a word string having many many word"
+answer = Freq_k(array,2)
+print(answer)
