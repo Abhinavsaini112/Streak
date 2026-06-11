79adeff90b89ab43b01847040390c10a3620d317
abhinavsaini112 <abhinavsaini8439@gmail.com>
2026-06-11 08:25:15 +0530

    Day 6 of 21 Days Challenge

---
+++ Leetcode/Pattern/Medium/ArrowPattern.py
@@ -0,0 +1,26 @@
+def ArrowPattern(num) :
+    n , m = (num + 1) // 2 , (num - 1) // 2
+
+    for i in range(n) :
+        for j in range(i): 
+            print(" ", end = '')
+
+        for k in range(i+1):
+            print('*', end = '')
+
+        print()
+    
+    for i in range(m):
+        for j in range(m-i-1,0,-1):
+            print(' ', end = '')
+        
+        for k in range(m-i,0,-1):
+            print('*', end = '')
+        
+        print()
+    
+    
+
+        
+value = int(input("Enter the value of n : "))
+ArrowPattern(value)
---
+++ Leetcode/Pattern/Medium/PyramidPattern.py
@@ -0,0 +1,23 @@
+def PyramidPattern(num):
+
+    for i in range(num):
+    
+        j = num - i - 1
+        while j > 0 :
+            print(' ', end = '')
+            j -= 1
+        
+        k = i + 1
+        while k > 0 :
+            print(k, end = '')
+            k -= 1
+        
+        l = k + 1
+        while l <= i :
+            print(l + 1, end = '')
+            l += 1
+        
+        print()
+
+value = int(input("Enter the value of n : "))
+PyramidPattern(value)
