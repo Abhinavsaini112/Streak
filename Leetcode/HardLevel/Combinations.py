c42e589de878fe1ed37b42e74eabb284204232ef
abhinavsaini112 <abhinavsaini8439@gmail.com>
2026-07-05 23:59:35 +0530

    yes

--- Leetcode/HardLevel/Combinations.py
+++ Leetcode/HardLevel/Combinations.py
@@ -8,10 +8,43 @@ def combinations(nums,k):
         
         for i in range(start , len(nums)):
             path.append(nums[i])
+
             backtracking(i+1,path)
+
             path.pop()
 
     backtracking(0 , [])
     return result
 
-print(combinations([1,2,3,4],2))
+print(combinations([1,2,3,4],3))
+
+'''Time Complexity
+
+Let
+n = len(nums)
+k = size of each combination
+
+The number of combinations is (nCk) = a
+
+For every combination, copying path takes O(k) = b time. 
+
+Time Complexity =O(a * b)'''
+
+'''Space Complexity
+
+Auxiliary space : is the extra or temporary memory space used by an algorithm during 
+its execution to solve a problem. It excludes the memory required to store the 
+original input data.
+
+Input Space: The memory required to hold the initial data provided to the algorithm.
+
+Space Complexity = Input Space + Auxiliary Space
+
+Auxiliary Space
+O(k)
+
+Total Space (including output)
+O(a * b)
+	​
+
+'''
--- Leetcode/HardLevel/Permutation.py
+++ Leetcode/HardLevel/Permutation.py
@@ -1,17 +1,21 @@
 def Permutation(nums):
     result = []
-    used = [False]*len(nums)
+    used = [False] * len(nums)
 
     def backtrack(path):
         if len(path) == len(nums):
-            result.append(path[:])
+            result.append(path.copy())
             return
+        
         for i in range(len(nums)):
             if used[i]:
                 continue
+
             used[i] = True
             path.append(nums[i])
+
             backtrack(path)
+
             path.pop()
             used[i] = False
         
@@ -20,6 +24,7 @@ def Permutation(nums):
 
 print(Permutation([1,2,3]))
 
+print()
 
 def Permutation_K(nums,k):
     result = []
@@ -34,13 +39,15 @@ def Permutation_K(nums,k):
         for i in range(len(nums)):
             if used[i]:
                 continue
+
             path.append(nums[i])
             used[i] = True
+
             backtracking(path)
+
             path.pop()
             used[i] = False
             
-    
     backtracking([])
     return result
 
@@ -50,7 +57,6 @@ print(Permutation_K([1,2,3,4],3))
 Total permutations generated are:
 P(n,k)= n!/(n-k)!
 ​	
- 
 For each permutation:
 copying path[:] takes:
 O(k)
@@ -58,3 +64,4 @@ So total time complexity:
 O((n!/(n-k)!)⋅k)
 
 Space Complexity is O(n). '''
+
