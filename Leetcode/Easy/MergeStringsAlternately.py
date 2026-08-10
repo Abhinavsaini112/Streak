3b3417a720103543af21720501b648af39007bc5
abhinavsaini112 <abhinavsaini8439@gmail.com>
2026-08-11 02:38:15 +0530

    Day 2 of 21 Days Challenge

--- Leetcode/Easy/MergeStringsAlternately.py
+++ Leetcode/Easy/MergeStringsAlternately.py
@@ -1,20 +1,24 @@
 def MergeStringsAlternately(word1,word2):
-    i=0
-    result=""
-    while i<len(word1) or i<len(word2):
-        if i<len(word1):
-            result+=word1[i]
-        if i<len(word2):
-            result+=word2[i]
-        i+=1
-    return result
+    i = 0
+    result = []
+    while i < len(word1) or i < len(word2):
+        if i < len(word1):
+            result.append(word1[i])
+        if i < len(word2):
+            result.append(word2[i])
+        i += 1
+
+    return "".join(result)
 
 
-word1='abc'
-word2='def'
-result=MergeStringsAlternately(word1,word2)
+word1 = 'abc'
+word2 = 'def'
+result = MergeStringsAlternately(word1,word2)
 print(result)
 
  
-'''Time Complexity is O(n+m) and Space Complexity is O(n+m) where n and m are the length of the string.'''
+'''Time Complexity is O(n+m) 
+ Space Complexity is O(n+m) 
+ where n and m are the length of the string.'''
 
+"Pattern you're learning: this is an example of two-pointer/index traversal + output construction."
--- Leetcode/Easy/MoveZeros.py
+++ Leetcode/Easy/MoveZeros.py
@@ -12,10 +12,22 @@ def MoveZeros(lst):
     while write < len(lst):
         lst[write] = 0
         write += 1
-
     return lst
-
 
 li = [0, 4, 1, 2, 0, 0, 2, 3, 1, 0]
 
 print(MoveZeros(li))
+
+'''Can you minimize the total number of operations done?'''
+
+def Movezeros(list):
+    write = 0 
+    for read in range(len(list)):
+        if list[read] != 0:
+            list[write], list[read] = list[read], list[write]
+            write += 1
+    return list
+
+li = [0, 4, 1, 2, 0, 0, 2, 3, 1, 0]
+
+print(Movezeros(li))
