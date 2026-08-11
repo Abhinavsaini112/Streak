5056888576f7f803bb5c2495b552b8833569012f
abhinavsaini112 <abhinavsaini8439@gmail.com>
2026-08-11 23:59:54 +0530

    Day 2 of 21 Days Challenge

---
+++ Leetcode/Easy/isSubsequence.py
@@ -0,0 +1,15 @@
+def IsSubsequence(li,s):
+    i = 0
+    j = 0
+    while i < len(li) and j < len(s):
+        if li[i] == s[j]:
+            j += 1
+        i += 1
+    return j == len(s)
+
+li = 'ahbgdc'
+subseq = 'agc'
+print(IsSubsequence(li,subseq))
+
+'''Time Complexity is O(n)
+Space Complexity is O(1)'''
